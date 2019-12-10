28035accb0e5e5e0c9a3a504c53fed07539f026a
andrey <annrra@gmail.com>
2019-12-10 11:48:08 +0200

    sass-loader and mini-css-extract-plugin added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './styles/style.scss';
+
 const App = ({ title }) =>
   <div>{title}</div>;
   
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,59 @@
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".css", ".scss"]
+  },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: ['babel-loader']
-      }
+        loader: 'babel-loader'
+      },
+      // Rules to load scss
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // fallback to style-loader in development
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
     ]
   },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  output: {
-    path: __dirname + '/public',
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
+  devtool: 'cheap-module-eval-source-map',
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+	new webpack.HotModuleReplacementPlugin(),
+	new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
   ],
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
   devServer: {
-    contentBase: './public',
+    contentBase: path.join(__dirname, 'public'),
     hot: true
   }
 };
